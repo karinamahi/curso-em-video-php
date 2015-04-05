eacc97c3fdd4d287a8f89a7053e2a69dfb79e4b9
Karina Mayumi Hirata <karinamahi@hotmail.com>
2015-04-05 19:29:40 -0300

    aula 3 - ola mundo

---
+++ ola-mundo/ola-mundo.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+	<head>
+		<meta charset="utf-8"/>
+		<title>Primeiro exemplo PHP</title>
+		<style>
+			h2 {
+				color: #80a2ff;
+				text-shadow: 1px 1px 1px black;
+			}
+
+		</style>
+	</head>
+
+	<body>
+		<h1>Testando PHP</h1>
+		<?php
+		echo "<h2>Olá, <br/> Mundo!<h2>";
+		?>
+
+
+	</body>
+</html>
