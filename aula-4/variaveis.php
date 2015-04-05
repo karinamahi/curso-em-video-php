ce055dc784658000bfc970dc49bf7767eb845f81
Karina Mayumi Hirata <karinamahi@hotmail.com>
2015-04-05 20:05:05 -0300

    aula 4 - variáveis em php

---
+++ aula-4/variaveis.php
@@ -0,0 +1,26 @@
+<DOCTYPE html>
+
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="_css/estilo.css"/>
+		<title></title>
+
+	</head>	
+
+	<body>
+		<div>
+			<?php
+			$idade = 36;
+			$nome = "Gustavo";
+			
+			echo $nome." tem ".$idade." anos! </br>";
+			echo "$nome tem $idade anos!"
+			
+
+		?>
+		</div>
+	</body>
+
+
+</html>
